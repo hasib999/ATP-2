8d19c27367c1669ca795d5ce79e484a8a0be897c
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-10-31 22:04:15 +0600

    new cls

---
+++ Fourth/Third/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Third.Models;
+
+namespace Third.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            /*if (Request.HttpMethod == "GET")
+            {
+                return View();
+            }
+            else
+            {
+                Session["name"] = Request["name"];
+                Session["username"] = Request["username"];
+                Session["password"] = Request["password"];
+                Session["blood"] = Request["blood"];
+                Session["gender"] = Request["gender"];
+                Session["dob"] = Request["dob"];
+                return RedirectToAction("Another");
+            }*/
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Person p) 
+        {
+            Session["name"] = p.name;
+            Session["username"] = p.username;
+            Session["password"] = p.password;
+            Session["blood"] = p.blood;
+            Session["gender"] = p.gender;
+            Session["dob"] = p.dob;
+            return RedirectToAction("Another");
+        }
+        public ActionResult Another()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fourth/Third/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Third.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string blood { get; set; }
+        public string gender { get; set; }
+        public string dob { get; set; }
+    }
+}
